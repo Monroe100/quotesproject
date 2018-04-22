a1b8a43f2ca39f6a0c01c0d733b9625587218aec
Monroe100 <michelle1hunnid@gmail.com>
2018-04-22 18:32:03 +0300

    made relevant changes

--- src/app/quotes-form/quotes-form.component.ts
+++ src/app/quotes-form/quotes-form.component.ts
@@ -6,8 +6,29 @@ import { Quote } from '../quotes'
   styleUrls: ['./quotes-form.component.css']
 })
 export class QuotesFormComponent implements OnInit {
-  newQuote = new Quote("", "", "")
-  constructor() { }
+  // quote["", "", ""]
+  myArr = [
+
+  ]
+  // new Quote(0, 'me', 'you', 'feeling lucky', 90, 30, new Date('2018-09-09'))
+  count: number = 0
+  upvotes: number = 0
+  downvotes: number = 0
+
+  formSubmit(x) {
+    this.myArr.push(new Quote(this.count++, x.value.author, x.value.name, x.value.quote, this.upvotes, this.downvotes, x.value.date))
+    // console.log(x.value.author, x.value.name, x.value.quote, x.value.date)
+
+  }
+  upVote() {
+    this.upvotes++
+  }
+  downVote() {
+    this.downvotes++
+  }
+  constructor() {
+    console.log(this.myArr)
+  }
 
   ngOnInit() {
   }
--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,3 +1,19 @@
-export class Quotes {
-  constructor(public Author: string, public Name: string, public Quote: string)
+export class Quote {
+  public id: number
+  public author: string
+  public name: string
+  public quote: string
+  public upvotes: number
+  public downvotes: number
+  public date: Date
+  constructor(id, author, name, quote, upvotes, downvotes, date) {
+    this.id = id
+    this.author = author
+    this.name = name
+    this.quote = quote
+    this.upvotes = upvotes
+    this.downvotes = downvotes
+    this.date = date
+  }
+
 }
